b3ed8d1b7dd3c851f82fc99e419c4f3f157e1fb8
“India1947” <prashantw677@gmail.com>
2024-06-16 23:18:28 +0530

    lab006 7 8 with practice

---
+++ 07062024/lab007.py
@@ -0,0 +1,19 @@
+import math
+
+prashant_marks = 90
+vishal_marks = 89
+
+#print("prashant is winner" if prashant_marks > vishal_marks else "vishal is winner")
+
+# OR
+
+if prashant_marks > vishal_marks:
+    print("prashant is winner")
+else:
+    print("vishal is winner")
+
+
+
+#================================================================
+
+
