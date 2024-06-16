5166ef26004af95a17686c6417beffcccc9c8f12
“India1947” <prashantw677@gmail.com>
2024-06-16 23:53:09 +0530

    lab009  assignment completed

--- 07062024/lab009.py
+++ 07062024/lab009.py
@@ -1,17 +1,35 @@
 #Task1
 # 1.Explain the difference between the = operator and the == operator in Python.
-# 2.What does the ** operator do in Python, and how is it used?
-# 3.What does the ^ operator do in Python, and in what context is it commonly used?
 
 1. '=' ==> work as an assignment operator. It assign to the variable
 
 '==' ==> work as a equal to operator. It returns true if the values are equal on left and right side or false return otherwis.
 
+# 2.What does the ** operator do in Python, and how is it used?
+
 2. '**' ==> work as an arithmatic operator in python often used for exponentiation.(it's a power operator that raises the number on left to the power of the number on its right')
+
+
+# 3.What does the ^ operator do in Python, and in what context is it commonly used?
 
 3. '^'  ==> The ^ operator does a binary xor. a ^ b will return a value with only the bits set in a or in b but not both.
 
 
 
+# Task2
+# 1. Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
 
+print(10 ** 2)
+# or
+
+r = pow(8, 3)
+print(r)
+
+# 2. Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+
+x = 20
+y = 20
+
+print("x is greater than y" if x >= y else "x is small")
 
